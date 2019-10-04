bb8190b6967426d0a82d518a22891b2b29e8c687
mervinkogei <vincent.kogei@yahoo.com>
2019-10-04 14:41:20 +0300

    add the validation to the contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $("form").submit(function(event){
+        var name= $("input#name").val();
+        alert("Hello Esteemed Customer " + name + ", " + "Thank you for contacting us !");
+
+        event.preventDefault();
+    });
+});
 //Business Logic
 
 var pizzaCart = (function() {
